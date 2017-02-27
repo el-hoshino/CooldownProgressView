f5cd233cae6e4e13ac09a537fc7f7daff384729e
史 翔新 <shi_xiangxin@mages.co.jp>
2017-02-27 14:21:58 +0900

    Init Xcode project

---
+++ CooldownProgressView/CooldownProgressView/CooldownProgressView.h
@@ -0,0 +1,19 @@
+//
+//  CooldownProgressView.h
+//  CooldownProgressView
+//
+//  Created by 史　翔新 on 2017/02/27.
+//  Copyright © 2017年 Crazism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CooldownProgressView.
+FOUNDATION_EXPORT double CooldownProgressViewVersionNumber;
+
+//! Project version string for CooldownProgressView.
+FOUNDATION_EXPORT const unsigned char CooldownProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CooldownProgressView/PublicHeader.h>
+
+
